8fb563c771613f9b8ee6396b282b448861f9f283
vova.kirdan@mail.ru <_4IDuzhSOe>
2025-07-19 14:08:50 +0300

    Initial Commit

---
+++ ZeroID/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZeroID
+//
+//  Created by Владимир Кирдан on 19.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZeroID/ZeroIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroIDApp.swift
+//  ZeroID
+//
+//  Created by Владимир Кирдан on 19.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ZeroIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
